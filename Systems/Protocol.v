493aeef86da1baf7fe7939ad99a49f7478cfeeef
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-07-02 12:43:34 +0100

    Implemented RoundChange world step

--- Systems/Protocol.v
+++ Systems/Protocol.v
@@ -20,13 +20,13 @@ RndGen -> (Hashed * seq Transaction * nat) -> OracleState -> (OracleState * Hash
 
 
 
-Record Adversary := Advrs {
+Record Adversary := mkAdvrs {
   adversary_local_transaction_pool: seq Transaction;
   adversary_local_message_pool: seq BlockChain;
   adversary_proof_of_work: nat;
 }.
 
-Record LocalState := LclSt {
+Record LocalState := mkLclSt {
   honest_current_chain: BlockChain;
   honest_local_transaction_pool: seq Transaction;
   honest_local_message_pool: seq BlockChain;
@@ -34,6 +34,12 @@ Record LocalState := LclSt {
 }.
 
 
+(* GlobalState consists of 
+      1. A sequence of LocalStates, and a boolean representing whether the state is corrupted
+      2. An address representing the currently executing entity - when addr == length of local states + 1,
+         the round is complete
+      3. A number representing the current round
+*)
 Definition GlobalState := ((seq (LocalState * bool)) * Addr * nat)%type.
 
 
@@ -44,5 +50,24 @@ Record World := mkWorld {
   world_transaction_pool: TransactionPool;
   world_inflight_pool: MessagePool;
   world_message_pool: MessagePool;
-  world_hash: RndGen -> (Hashed * seq Transaction * nat) -> OracleState -> (OracleState * Hashed)
+  world_hash: OracleState
 }.
+
+(* A round is complete if the currently_active index is one greater than the length of the actors array *)
+Definition round_ended (w: World) :=
+(world_global_state w).1.2 = ((length (world_global_state w).1.1) + 1)
+. 
+
+
+(* Implements the round robin - each actor activated once a round mechanism *)
+Definition update_round (state : GlobalState) : GlobalState := let: (actors, active, round) := state in 
+  if (eqn active (length actors).+1) 
+  then (actors, 0, round.+1) 
+  else (actors, active.+1, round).
+
+
+Inductive world_step (w w' : World) (random : RndGen) : Prop :=
+   RoundChange of round_ended w &
+  let: new_state := update_round (world_global_state w) in
+  w' = mkWorld new_state (world_transaction_pool w) (world_inflight_pool w) (world_message_pool w) (world_hash w)
+.    
