31988303b9a178df4127735e1fe5b39606883bfb
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-07-03 12:57:53 +0100

    fixed rndgen to incorporate all random aspects of an execution

--- Structures/BlockChain.v
+++ Structures/BlockChain.v
@@ -7,14 +7,42 @@ Require Import ssreflect ssrbool ssrnat eqtype ssrfun seq path.
 Definition Hashed := nat.
 (* Simmilarly, Addr must be an index into the honest actors, thus not a parameter*)
 Definition Addr := nat.
-(* RndGen will be passed down from the probabilistic component and used to simulate any probabilistic components *)
-Definition RndGen := nat.
+
 
 Parameter Transaction : eqType.
 (* determines whether a transaction is valid or not with respect to another sequence of transactions*)
 Parameter Transaction_valid : Transaction -> seq Transaction -> bool. 
 
 Definition TransactionPool := seq (Transaction * (seq Addr)).
+
+
+(* RndGen will be passed down from the probabilistic component and used to simulate any probabilistic components *)
+(* RndGen encodes the random/unknown aspects of the system 
+  specifically, 
+      - creating transaction
+      - droppping transactions 
+      - minting blocks
+      - adversary corrupting
+      - adversary broadcasting
+          - *)
+(* together with the restrictions imposed by world step, all valid
+   sequences of actions can be considered *)
+Inductive RndGen  := 
+    (* used by both Honest and Adversary Parties to generate transactions *)
+    | TransactionGen of Transaction 
+    | TransactionDrop of nat
+    (* used by both Honest and Adversary Parties to mint blocks*)
+    (* The nat represents the return value of the random oracle if the block is new*)
+    (* this nat will be probabilistically analyzed*)
+    | MintBlock of nat  
+    (* Used to represent the adversary corrupting players - nat is an index into
+       which player to corrupt*)
+    | AdvCorrupt of nat
+    (* used by adversary parties to broadcast chains - nat is an index into 
+       the adversaries local blockchain pool*)
+    | AdvBroadcast of nat
+    | AdvTarget of  seq nat.
+
 
 Record Block := Bl {
   block_link: Hashed;
