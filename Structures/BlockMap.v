2b5fb83925428e03dd04d201bab32188e0a0c1c5
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-07-10 11:05:38 +0100

    modified Blockmap to be a list for easy iteration

--- Structures/BlockMap.v
+++ Structures/BlockMap.v
@@ -5,40 +5,34 @@ Require Import FMapAVL.
 From Probchain
 Require Import BlockChain InvBlock.
 
-Module M := FMapAVL.Make(Block_as_OT).
-Definition BlockMap := M.t (bool * nat).
+(* Rather than using a map, we will make our own record to 
+   map blocks to their meta information - mainly to allow 
+   for easy iteration*) 
 
-
-Definition BlockMap_new : BlockMap.
-Proof.
-  (* TODO(Kiran): complete this proof *) 
- Qed.
-
-
-Definition BlockMap_find k (b : BlockMap) := M.find k b.
-
-
-(* Used to record additional state of blocks *)
-(* The extra logic is so that an adversary can not change an honest block into an adversarial block by hashing it*)
-Definition BlockMap_put  block (block_info: bool * nat) (b : BlockMap) := 
-    let (new_is_adversarial, last_round) := block_info in
-        if new_is_adversarial 
-            then
-            (* if it is adversarial, check if the last entry was adversarial *)
-                match BlockMap_find block b with
-                    | None => M.add block block_info b
-                    | Some old_block_info => 
-                            let (old_is_adversarial, _) := old_block_info in
-                                if old_is_adversarial 
-                                    (* if the old one was also adversarial, then update *)
-                                    then M.add block block_info b
-                                    (* if the old one was honest, then don't change it*)
-                                    else b
-                end
-            else
-            (* If it isn't adversarial, then just put it in*)
-                M.add block block_info b.
+Definition BlockMap := seq (Block * bool * nat).
 
 
 
+Definition BlockMap_find (bl : Block) (map : BlockMap) : option (bool * nat):= 
+    foldr
+        (fun (new_pair : Block * bool * nat) (acc : option (bool * nat)) =>
+            match acc with
+                | Some val => acc
+                | None =>
+                    let: (bl', is_corrupt, round) := new_pair in
+                    if (bl' == bl)
+                        then Some (is_corrupt, round)
+                        else None
+                end)
+        None
+        (map).
 
+
+
+Definition BlockMap_put_honest (bl : Block) (round: nat) (map: BlockMap) :=
+    (bl, false, round) :: map.
+        
+Definition BlockMap_put_adversarial (bl : Block) (round : nat) (map: BlockMap):=
+    (bl, true, round) :: map.
+
+Definition BlockMap_new : BlockMap := nil.
