859ed716b9682d05b38ca71230a1048c33ad3e56
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-07-18 12:40:50 +0100

    wrote the base axioms required  for probability using bigop

--- Probability/Dist.v
+++ Probability/Dist.v
@@ -1,8 +1,25 @@
 
 From mathcomp.ssreflect
-Require Import ssreflect ssrbool ssrnat seq ssrfun eqtype.
+Require Import ssreflect ssrbool ssrnat seq ssrfun eqtype bigop.
 
-From mathcomp.ssreflect
-Require Import all_ssreflect.
+From mathcomp.algebra
+Require Import rat.
 
-Definition Dist A := A -> Rat.
+Set Implicit Arguments.
+
+Definition Dist A := A -> rat.
+
+Section definitions.
+    (* A distribution on A maps values in A to a probability*)
+    Variable A : eqType.
+    Variable P : Dist A.
+    Variable sample_space : seq A.
+
+    Definition prob event := \big[addq/0%Q]_(i <- event) (P i).
+    Notation "Pr[ x ]" := (prob x) .
+
+    Hypothesis wf_sample_space : Pr[ sample_space ] = 1%Q.
+    Hypothesis wf_probability :  forall x, x \in sample_space -> Pr[ [:: x] ] = 0%Q.
+
+
+End definitions.
