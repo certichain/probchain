c28a84ca5021dc0a731ac18e96a5381a255ec192
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-07-19 14:32:15 +0100

    implemented getSupport

--- Probability/Comp.v
+++ Probability/Comp.v
@@ -1,5 +1,5 @@
 From mathcomp.ssreflect
-Require Import ssreflect ssrbool ssrnat seq ssrfun eqtype bigop fintype.
+Require Import ssreflect ssrbool ssrnat seq ssrfun eqtype bigop fintype choice.
 
 From infotheo
 Require Import proba.
@@ -10,13 +10,40 @@ Set Implicit Arguments.
 (* Computation definition - from FCF*)
 Section Comp.
 
-
+(* 
     Inductive Comp (A : finType) :  Type :=
         | Ret : forall a : A, Comp A 
         | Bind : forall B, Comp B -> (B -> Comp A) -> Comp A
         | Repeat :  Comp A -> pred A -> Comp A
         | Rnd : forall n : nat, Comp A.
+ *)
 
+
+    Inductive Comp : finType -> Type :=
+        | Ret : forall (A : finType) (a : A), Comp A 
+        | Bind : forall (A B : finType), Comp B -> (B -> Comp A) -> Comp A
+        | Repeat : forall (A : finType),  Comp A -> pred A -> Comp A
+        | Rnd : forall (A : finType) (n : nat), Comp A.
+
+
+
+
+    Fixpoint getSupport(A : finType) (c : Comp A) : list A :=
+        match c with
+            | Ret _ a => [:: a]
+            | Bind _ _ c1 c2 => 
+            (flatten 
+                (map 
+                    (fun b => (getSupport (c2 b)))
+                    (getSupport c1)
+                )
+            )
+            | Repeat _ c P => (filter P (getSupport c))
+            | Rnd A n => (flatten (map (fun x => match pickle_inv A x with 
+                                                        | Some value => [:: value]
+                                                        | None => [::]
+                                                        end) (index_iota 0 n)))
+        end.
 
 
 End Comp.
