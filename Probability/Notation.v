93a6c8b8b382bc5208a288a348e769356620c424
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-07-23 11:54:41 +0100

    changed Prop types to bool

--- Probability/Notation.v
+++ Probability/Notation.v
@@ -1,7 +1,7 @@
 Set Implicit Arguments.
 
 From mathcomp.ssreflect
-Require Import ssreflect ssrnat seq ssrbool fintype.
+Require Import ssreflect ssrnat seq ssrbool ssrfun fintype choice.
 From Probchain
 Require Import Comp.
 
@@ -10,7 +10,7 @@ Notation "'ret' v" := (Ret _ v) (at level 75).
 Notation "{ 0 , 1 } ^ n" := (Rnd _ (2^n))
     (right associativity, at level 77).
 
-Notation "{ 0 , 1 }" := (Rnd _ 2) 
+Notation "{ 0 , 1 }" := (Rnd _ _ 2) 
     (right associativity, at level 75).
 
 Notation "x <-$ c1 ; c2" := (@Bind _ _ c1 (fun x => c2))
@@ -21,6 +21,19 @@ Notation "x <- e1 ; e2" := ((fun x => e2) e1)
 
 Check 'I_3.
 
+Section temp.
+    Definition A := ordinal 3.
+    Print Canonical Projections.
+    Variable a : A.
+    Locate "#| _ |".
+
+
+    Lemma is_valid : 2 < #| A |.
+
+    Definition example : Comp A := 
+        x <- 3; 
+        y <-$ {0,1};
+        ret y.
 
 About Rnd.
 
