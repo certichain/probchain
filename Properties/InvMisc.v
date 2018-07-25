c729d1a1eccd3dabf3f95778144d2d482f5277c4
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-07-25 16:36:34 +0100

    removed hash-triple-as-ot will need to add custom (finite) map implementation

--- Properties/InvMisc.v
+++ Properties/InvMisc.v
@@ -9,35 +9,6 @@ From Probchain
 Require Import BlockChain.
 
 
-Module Hash_Triple_as_OT <: OrderedType.
-  
-  Definition t : Type := (Hashed * seq.seq Transaction * nat).
-  
-  Definition eq : Hashed * seq.seq Transaction * nat ->
-       Hashed * seq.seq Transaction * nat ->
-       Prop := fun (x y : (Hashed * list Transaction * nat)) =>
-      Nat_as_OT.eq (fst (fst x)) (fst (fst y)) /\
-      eq (snd (fst x)) (snd (fst y)) /\
-      Nat_as_OT.eq   (snd x) (snd y).
-      
-
-   Definition lt (x y : (Hashed * list Transaction * nat)) :=
-    Nat_as_OT.lt (fst (fst x)) (fst (fst y)) \/
-    (Nat_as_OT.eq (fst (fst x)) (fst (fst y))) /\ (Nat_as_OT.lt (snd x) (snd y)).    
-      
-     Definition eq_refl : forall x : (Hashed * list Transaction * nat), eq x  x. Proof. Admitted.
-     
-     Definition eq_sym : forall x y : (Hashed * list Transaction * nat), eq x y -> eq y x. Proof. Admitted.
-
-     Definition eq_trans : forall x y z : (Hashed * list Transaction * nat), eq x y -> eq y  z -> eq x  z. Proof. Admitted.
-
-     Parameter lt_trans : forall x y z : (Hashed * list Transaction * nat), lt x y -> lt y z -> lt x z.
-     Parameter lt_not_eq : forall x y : (Hashed * list Transaction * nat), lt x y -> ~ eq x y.
-     Definition compare : forall x y : (Hashed * list Transaction * nat), OrderedType.Compare lt eq x y. Proof. Admitted.
-     Definition eq_dec : forall n m : (Hashed * list Transaction * nat), {eq n m} + {not (eq n m)}. Proof. Admitted.
-
-End Hash_Triple_as_OT.
-
 
 (* Couldn't find a remove_nth function in stdlib or ssreflect*)
 Fixpoint rem_nth {A:Type} (n : nat) (ls : list A) : list A := 
