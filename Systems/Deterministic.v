fc232431b892991b923ac1933ceefa43505c4519
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-07-31 13:16:18 +0100

    implemented probabilistic adversary attempt hash

--- Systems/Deterministic.v
+++ Systems/Deterministic.v
@@ -5,7 +5,7 @@ Require Import Comp Notationv1 BlockChain Protocol OracleState BlockMap InvMisc 
 
 
 From mathcomp.ssreflect
-Require Import ssreflect ssrbool ssrnat eqtype fintype choice ssrfun seq path.
+Require Import ssreflect ssrbool ssrnat eqtype fintype finfun choice ssrfun seq path.
 
 From mathcomp.ssreflect
 Require Import tuple.
@@ -87,61 +87,49 @@ Definition honest_attempt_hash
       (* this is an invalid state - head should always return a value *)
       else None.
 
-          (* let: (new_oracle_state, hash) := hash random_value (value, selected_transactions, proof_of_work) oracle_state in
-          if hash < T_Hashing_Difficulty then
-            (* New block has been minted *)
-            let: new_block := Bl nonce value selected_transactions proof_of_work in
-            let: new_chain := fixlist_insert best_chain new_block in
-            let: new_state :=
-                  mkLclSt 
-                    new_chain
-                    remaining
-                    (fixlist_rem (honest_local_message_pool state) best_chain)
-                    (Ordinal valid_Maximum_proof_of_work) in (* reset the proof of work *)
-            (new_state, Some(BroadcastMsg new_chain), new_oracle_state, Some new_block, Some new_chain)
-          else
-            (* Constructed block did not meet the difficulty level *)
-            (* if the longest chain is actually the current chain *)
-            if best_chain == (honest_current_chain state)
-              (* then the only thing to change is to increment the proof of work*)
-              then 
-               let: new_state := 
-                    mkLclSt 
-                      (honest_current_chain state) 
-                      (honest_local_transaction_pool state)
-                      (honest_local_message_pool state)
-                      (mod_incr Maximum_proof_of_work valid_Maximum_proof_of_work (honest_proof_of_work state)) in
-              (new_state, None, new_oracle_state, None, None)
-            else 
-              (* Otherwise we need to move the best chain from the message pool to current*)
-              let: new_state := 
-                    mkLclSt 
-                      best_chain 
-                      (honest_local_transaction_pool state)
-                      (fixlist_rem (honest_local_message_pool state) best_chain)
-                      (mod_incr Maximum_proof_of_work valid_Maximum_proof_of_work (honest_proof_of_work state))
-                      in
-              (new_state, Some(BroadcastMsg best_chain), new_oracle_state, None, None)
-        else 
-          if best_chain == (honest_current_chain state)
-            then (state, None, oracle_state, None, None)
-          else 
-            let: new_state := 
-                  mkLclSt 
-                    best_chain 
-                    (honest_local_transaction_pool state)
-                    (fixlist_rem (honest_local_message_pool state) best_chain )
-                    (honest_proof_of_work state) in
-            (new_state, Some(BroadcastMsg best_chain), oracle_state, None, None)
-    .
- *)
+
+
 Definition adversary_attempt_hash 
     (adversary : Adversary adversary_internal_state) 
     (inflight_messages : MessagePool) 
-    (hash_state : Hashed * OracleState) : (Adversary adversary_internal_state * OracleState * option Block) :=
+    (hash_state : Hashed * OracleState) : option (Comp [finType of (Adversary adversary_internal_state * OracleState * option Block)]) :=
   let: (new_hash, oracle_state) := hash_state in
   (* Adversary can generate the block however they want *)
   let: (adversary_partial, (nonce, hashed, transactions, pow)) := (adversary_generate_block adversary) (adversary_state adversary) inflight_messages in
+    Some(hash_result <-$ hash (nonce, hashed, transactions, pow) oracle_state;
+    result <- 
+      let (new_oracle_state, result) := hash_result in
+      let: adversary_new_state := (adversary_provide_block_hash_result adversary) adversary_partial (nonce, hashed, transactions, pow) result in
+        if result < T_Hashing_Difficulty 
+          then 
+            let: block := Bl nonce hashed transactions pow in
+            let: new_adv :=  mkAdvrs
+              adversary_new_state
+              (adversary_state_change adversary)
+              (adversary_insert_transaction adversary)
+              (adversary_insert_chain adversary)
+              (adversary_generate_block adversary)
+              (adversary_provide_block_hash_result adversary)
+              (adversary_send_chain adversary)
+              (adversary_send_transaction adversary)
+              (adversary_last_hashed_round adversary) in
+                (new_adv, new_oracle_state, Some block)
+          else 
+            let: new_adv :=  mkAdvrs 
+              adversary_new_state
+              (adversary_state_change adversary)
+              (adversary_insert_transaction adversary)
+              (adversary_insert_chain adversary)
+              (adversary_generate_block adversary)
+              (adversary_provide_block_hash_result adversary)
+              (adversary_send_chain adversary)
+              (adversary_send_transaction adversary)
+              (adversary_last_hashed_round adversary) in
+                (new_adv, new_oracle_state, None);
+          ret result).
+
+
+
   let: (new_oracle_state, result) := hash new_hash (hashed, transactions, pow) oracle_state in
   let: adversary_new_state := (adversary_provide_block_hash_result adversary) adversary_partial (nonce, hashed, transactions, pow) result in
   (* let: adversary_new_state := adversary_partial in *)
