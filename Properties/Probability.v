0c06225484c132d65c903503e3b7aeba7862a877
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-08-01 15:31:12 +0100

    wrote initial formulation of probabilistic chain quality property

--- Properties/Probability.v
+++ Properties/Probability.v
@@ -1,6 +1,9 @@
 From mathcomp.ssreflect
 Require Import ssreflect ssrbool ssrnat seq ssrfun eqtype bigop fintype choice.
 
+From mathcomp.ssreflect
+Require Import tuple.
+
 Require Import Reals Fourier FunctionalExtensionality.
 From infotheo
 Require Import proba ssrR Reals_ext logb ssr_ext ssralg_ext bigop_ext Rbigop .
@@ -22,7 +25,29 @@ Definition is_valid_schedule (schedule : seq.seq RndGen) :=
 
 (* Probability a given schedule is valid? *)
 Definition p_valid_schedule  (schedule : seq.seq RndGen) :=
-    evalDist (is_valid_schedule schedule) true  .
+    evalDist (is_valid_schedule schedule) true .
 
-true.
-Locate "Pr[ _ ]".
+    Variable probability_constant : R.
+
+Definition has_chain_quality_property (s: seq.seq RndGen) (l u : nat) (agent : 'I_n_max_actors) :=
+    o_current_w <-$ world_step initWorld s;
+    result <- if o_current_w is Some(current_w) then (((fixlist_length (honest_current_chain (fst (tnth (world_actors current_w) agent)))) > l)%nat &&
+    chain_quality_prop_agent current_w l u agent) else false;
+    ret result.
+
+Definition p_has_chain_quality_property (s : seq.seq RndGen) (l u : nat) (agent : 'I_n_max_actors) :=
+    evalDist (has_chain_quality_property s l u agent) true.
+
+
+
+Lemma p_chain_quality (l u : nat) : forall  (s: seq.seq RndGen) (agent : 'I_n_max_actors),
+   (* Forall schedules, *)
+   (* if the schedule is capable of producing a world *)
+    (p_valid_schedule s > R0) ->
+    (* Pr( result_w is some AND has_chain_quality_property ) / Pr (result_w is some) = *)
+    (* Pr (result_w has chain_quality_property, given that result_w is some) *)
+    (* The probability of recieving a world with the chain quality property, given
+    that world step is done with a valid sequence is equal to probability constant*)
+    (p_has_chain_quality_property s l u agent) / (p_valid_schedule s) = probability_constant.
+    Admitted.
+
