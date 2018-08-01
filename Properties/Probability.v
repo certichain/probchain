9773f711de0ca72aade38976e558d2f20a108c3f
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-08-01 17:13:59 +0100

    formulated unique round lemma

--- Properties/Probability.v
+++ Properties/Probability.v
@@ -69,3 +69,122 @@ Lemma p_chain_quality (l u : nat) : forall  (s: seq.seq RndGen) (agent : 'I_n_ma
     (p_has_chain_quality_property s l u agent) / (p_chain_quality_givens s l u agent) = probability_constant.
     Admitted.
 
+
+
+
+
+    About existsb.
+    Locate "_ > _".
+    Print Rlt.
+    Locate "^~".
+
+(* Definition passes_through_consecutive (w w' wfinal : World) (schedule : seq.seq RndGen) :=
+    (existsb (fun (n : nat) =>
+        ((evalDist  
+         (world_step initWorld (take n schedule)) (Some w)) > R0) &&
+        ((evalDist
+            (world_step w (take 1 (drop n schedule))) (Some w')) > R0) &&
+        ((evalDist
+            (world_step w' (drop n.+1 schedule)) (Some wfinal)) > R0)) (iota 0 (length schedule))). *)
+
+(* Definition common_prefix_givens (schedule : seq.seq RndGen) (k r1 r2 : nat) (a1 a2 : 'I_n_max_actors) (c1 c2 : BlockChain) :=
+    w' <-$ world_step initWorld schedule;
+    r <-
+        if w' is Some(current_w) then
+            (world_round_no current_w >= r2) &&
+            ~~ (actor_n_is_corrupt current_w a1) &&
+            ~~ (actor_n_is_corrupt current_w a1) &&
+    
+    (* exists consequtive rounds w' wr1 at r1 in which chain1 is adopted *)
+   (existsb (w' wr1 : World), 
+    (adopt_at_round w' wr1 c1 (widen_ord (leq_addr _ _) a1) r1) && 
+
+    )
+  (existsx (w'' wr2 : World), 
+  (* reachable initWorld w'' -> reachable w'' wr2 -> reachable wr2 current_w ->   *)
+    adopt_at_round w'' wr2 c2 (widen_ord (leq_addr _ _) a2) r2) ->
+
+        else false;
+    ret w.
+
+
+
+
+
+
+
+Definition common_prefix_property (current_w : World) (k r1 r2 : nat) (a1 a2 : 'I_n_max_actors) (c1 c2 : BlockChain) :=
+  (* current w is valid *)
+  (world_round_no current_w) >= r2 ->
+  r1 <= r2 ->
+  (a1 < n_max_actors) -> (a2 < n_max_actors) ->
+  ~~ (actor_n_is_corrupt current_w a1) -> ~~ (actor_n_is_corrupt current_w a1) ->
+  (* players a1 a2 adopting the chains at rounds r1, r2 *)
+  (exists (w' wr1 : World), 
+  (* reachable initWorld w' -> reachable w' wr1 -> reachable wr1 current_w ->   *)
+    adopt_at_round w' wr1 c1 (widen_ord (leq_addr _ _) a1) r1) ->
+  (exists (w'' wr2 : World), 
+  (* reachable initWorld w'' -> reachable w'' wr2 -> reachable wr2 current_w ->   *)
+    adopt_at_round w'' wr2 c2 (widen_ord (leq_addr _ _) a2) r2) ->
+  (* then pruning k blocks from the head of c1 is a subsequence of c2*)
+  prefix (drop k c1) c2.
+
+ *)
+
+ Definition unique_round_givens (schedule : seq.seq RndGen) (n : nat) (chain : BlockChain) :=
+    o_w' <-$ world_step initWorld schedule;
+    r <- if o_w' is Some(w) then
+        (chain \in (fixlist_unwrap (world_chain_history w))) &&
+        (fixlist_length chain > n)%nat &&
+        (nth_block_is_honest chain n w) &&
+        (nth_block_hashed_in_a_uniquely_successful_round w chain n)
+
+    else false;
+    ret r.
+
+ Definition p_unique_round_givens (schedule : seq.seq RndGen) (n : nat) (chain : BlockChain) :=
+    evalDist (unique_round_givens schedule n chain) true.
+
+Definition is_unique_round (schedule : seq.seq RndGen) (n : nat) (chain : BlockChain) :=
+    o_w' <-$ world_step initWorld schedule;
+    r <- if o_w' is Some(w) then
+        (chain \in (fixlist_unwrap (world_chain_history w))) &&
+        (fixlist_length chain > n)%nat &&
+        (nth_block_is_honest chain n w) &&
+        (if (nth_block_hashed_in_a_uniquely_successful_round w chain n) is Some(value) then
+            (all (fun other_chain => 
+                    if other_chain \in (fixlist_unwrap (world_chain_history w)) then
+                        if (fixlist_length other_chain > n)%nat then
+                            ((nth_block_is_adversarial w other_chain n) ||
+                            (nth_block_equals w other_chain n (nth_block w chain n)))
+                        else true
+                    else true
+                ) (fixlist_unwrap (world_chain_history w)))
+        else false)
+    else false;
+    ret r.
+
+Definition p_is_unique_round (schedule : seq.seq RndGen) (n : nat) (chain : BlockChain) :=
+    evalDist (is_unique_round schedule n chain) true.
+
+
+Lemma p_unique_round : forall  (s: seq.seq RndGen) (n : nat) (chain : BlockChain),
+   (* Forall schedules, *)
+   (* if the schedule is capable of producing worlds satisfying the givens *)
+    (p_unique_round_givens s n chain > R0) ->
+    (* Pr( givens_of result_w AND has_chain_quality_property ) / Pr (givens_of result_w ) = *)
+    (* Pr (result_w has chain_quality_property, given the givens) *)
+
+    (p_is_unique_round s n chain) / (p_unique_round_givens s n chain) = probability_constant.
+    Admitted.
+
+
+
+Lemma unique_round (w : World) (n : nat) (chain : BlockChain) :
+    chain \in (fixlist_unwrap (world_chain_history w)) -> length chain > n -> nth_block_is_honest chain n w  -> nth_block_hashed_in_a_uniquely_successful_round w chain n ->
+    (forall (other_chain : BlockChain), 
+    other_chain \in (fixlist_unwrap (world_chain_history w)) -> 
+    length other_chain > n -> 
+    nth_block_is_adversarial w other_chain n  \/ nth_block_equals w other_chain n (nth_block w chain n)).
+A
+
