80d915f502825cae2defda6f3e811df72a6ae65b
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-09-14 17:50:50 +0100

    completed 1 of the remaining 3 main lemmas

--- Properties/Probability.v
+++ Properties/Probability.v
@@ -2462,7 +2462,83 @@ Proof.
 
    (* successful attempt with non-last entry *)
 
-Admitted.
+      rewrite/honest_mint_succeed_update.
+      case Hhon_max_valid: (honest_max_valid _) => [chain_pool Hchain_pool] //=.
+      case Hupdated_chain: (fixlist_enqueue _ ) => [new_chain old_block] //=.
+      move=> H_has_chain_length_l_at_r' His_corrupted.
+      move=> s r Hr_is_valid Hs_eq_rdelta Hexecuted_to_s.
+      move/eqP: Heqn; move: Hwactive Hprf_ltn  H_has_chain_length_l_at_r'.
+
+
+      (* now - let's eradicate a_addr, and replace all instaces with w_addr *)
+      case Hcurr_round_value: ([[w'.state].#active])
+      => [w_state_active Hw_state_prf] //= Haddr_eq.
+      move: Hactive_state_eq Hactive_hlink Hnew_blockcontents Hmaxvld 
+                            Hexecuted_to_s .
+      move: Haddr_eq Hhon_max_valid.
+      case: a_addr => [a_addr Ha_addr] //= Hweq .
+      move: Ha_addr .
+      rewrite -Hweq => //=.
+      clear Hweq a_addr.
+      case Hw_round_eq: ([[w'.state].#round]) => [w_state_round Hwstatrnd] //=.
+      move: Hs_eq_rdelta Hr_is_valid.
+      case: s => [s Hsvld]; case: r => [r Hrvld] //=.
+      move=> Hs_eq_rdelta Hr_is_valid Hwstt_prf Hhon_max_valid Htnthactive_eq
+                         Hactive_hlink Hnew_blockcontents Hmxvld Hexecuted_to_s.
+      move=> Hnxt_active_ltn Ho_addr_has_chain Hwstatenotlast.
+      (* we also need to clean up the context and drop some of the lemmas we don't care for *)
+      move: Ho_addr_has_chain.
+      move => /orP [Ho_addr_has_chain | /andP [_ Hleq]]; last first.
+        by apply/orP; right.
+      clear Hltn.
+      move: Ho_addr_has_chain.
+      rewrite fixlist_insert_rewrite.
+      rewrite  has_rcons =>
+      /orP [/andP [Hlen_eq /andP [/eqP Hcurrent_round_eq /eqP Ho_addr_eq_active ]] | Ho_addr_has_chain].
+        move: (Hw_round_eq).
+        rewrite Hcurrent_round_eq => Heq.
+        move: Hr_is_valid.
+        move: (Hw_round_eq).
+        rewrite Heq => //=.
+        move: IHw' Hcurrent_round_eq Heq.
+        case: r' => [r' Hr'vld] //=.
+        move=> IHw' Hcurrent_round_eq Heq //= Hrordeq.
+        move: (Hrordeq) => [] {1} ->.
+        move: (Hexecuted_to_s).
+        have Hrlts: (r <= s)%nat.
+          rewrite -Hs_eq_rdelta.
+          by apply leq_addr.
+        by move=> /leq_trans H /H; clear H;
+            move: (Hrlts); move=> /ltnSn_eq H /H; clear H => /eqP Hseqr;
+            move: Hs_eq_rdelta; rewrite Hseqr => /eqP/addn_eq0/eqP Hdlta //=; move: delta_valid;
+            rewrite Hdlta ltnn.
+
+      rewrite has_rcons.
+      rewrite -orb_assoc; apply/orP; right.
+      apply IHw' with (s := (Ordinal Hsvld)) (r := (Ordinal Hrvld)) => //=.
+        by apply/orP; left.
+      have Hwtoord: (eq_op w_state_active (Ordinal Hwstt_prf)).  by[].
+      case Heq_active : (eq_op w_state_active  o_addr);
+        move: His_corrupted ;
+        rewrite Hcurr_round_value //=;
+        move: Hwtoord =>/eqP {1}->.
+
+        rewrite tnth_set_nth_eq //=. move =>Hanot_corrupt; move: (prf'); move/eqP: Heq_active => <- Vprf.
+        by rewrite (proof_irrelevance _ Vprf Hwstt_prf) Htnthactive_eq.
+        by move/eqP: Heq_active => ->.
+      rewrite tnth_set_nth_neq //=. 
+        by move/eqP: Heq_active =>/nesym/eqP.
+
+      by rewrite Hw_round_eq //=.
+      by move/Rlt_not_eq/nesym/world_step_adoption_history_top_heavy:Hprw'. 
+      by move/Rlt_not_eq/nesym/world_step_adoption_history_overflow_safe: Hprw'.
+Qed.
+
+
+
+
+
+
 
 
 
@@ -2577,6 +2653,7 @@ Proof.
     move/negP/negP: Hltn; rewrite -{1}ltnNge => Hltn.
     rewrite { 2 3 7 }Hltn => prf'.
     by move=> /deliver_messages_preserves_honest .
+    rewrite /world_executed_to_round.
     admit.
   (* adversary end case *)
     move=> IHw' Hprw' Hacthaschain Hhon  s Hpradv.
@@ -2652,6 +2729,7 @@ Lemma chain_growth_direct_weaken sc w l (r : 'I_N_rounds) s : forall Hsvddelta H
 Proof.
   (* TODO: Complete this proof *)
 Admitted.
+
 
 (*
   if round s - delta is bounded successful, then ~~ Xi for i in (s - 2 * delta,....s - delta - 1),
